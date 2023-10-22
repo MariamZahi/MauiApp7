ce740a41c9de278c7ffdb753de5c80ec56a3e2f2
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-10-22 02:53:11 -0400

    Model Class Created

---
+++ MauiApp7/Platforms/Android/Resources/CarMaintenanceRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp7.Platforms.Android.Resources
+{
+    internal class CarMaintenanceRecord
+    {
+ 
+            public string Id { get; set; }
+            public string ServiceType { get; set; }
+            public DateTime Date { get; set; }
+            public int Mileage { get; set; }
+            public string Notes { get; set; }
+        }
+    }
---
+++ MauiApp7/Platforms/Android/Resources/ICarMaintenanceRecord.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp7.Platforms.Android.Resources
+{
+    public interface ICarMaintenanceRecord
+    {
+        DateTime Date { get; init; }
+        string Id { get; init; }
+        int Mileage { get; init; }
+        string Notes { get; init; }
+        string ServiceType { get; init; }
+
+        void Deconstruct(out string Id, out string ServiceType, out DateTime Date, out int Mileage, out string Notes);
+        bool Equals(CarMaintenanceRecord.CarMaintenanceRecord other);
+        bool Equals(object obj);
+        int GetHashCode();
+        string ToString();
+    }
+}
